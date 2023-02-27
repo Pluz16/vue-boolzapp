c57faf1a7ddbb4e684137350f91931781a97cb00
Francesco Punta <puntafrancesco@gmail.com>
2023-02-27 09:23:07 +0100

    problemi visualizzazione vue

--- js/app.js
+++ js/app.js
@@ -4,85 +4,167 @@
 // ● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
 // nome e immagine di ogni contatto
 
-const contacts = [
-    {
-        name: 'Michele',
-        avatar: '_1',
-        visible: true,
-        messages: [
-            {
-                date: '10/01/2020 15:30:55',
-                message: 'Hai portato a spasso il cane?',
-                status: 'sent'
-            },
-            {
-                date: '10/01/2020 15:50:00',
-                message: 'Ricordati di dargli da mangiare',
-                status: 'sent'
-            },
-            {
-                date: '10/01/2020 16:15:22',
-                message: 'Tutto fatto!',
-                status: 'received'
-            }
-        ],
-    },
-    {
-        name: 'Fabio',
-        avatar: '_2',
-        visible: true,
-        messages: [{
-            date: '20/03/2020 16:30:00',
-            message: 'Ciao come stai?',
-            status: 'sent'
+Vue.config.devtools = true;
+
+const app = new Vue ({
+    el: "#root",
+    data: {
+        user: {
+            name: "Matteo",
+            surname: "De Marchi",
+            userAvatar: "_io",
         },
+        contacts: [
             {
-                date: '20/03/2020 16:30:55',
-                message: 'Bene grazie! Stasera ci vediamo?',
-                status: 'received'
+                name: 'Michele',
+                avatar: 'avatar_1.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di dargli da mangiare',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                ],
             },
             {
-                date: '20/03/2020 16:35:00',
-                message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                status: 'received'
-            }
-        ],
-    },
-    {
-        name: 'Samuele',
-        avatar: '_3',
-        visible: true,
-        messages: [{
-            date: '28/03/2020 10:10:40',
-            message: 'La Marianna va in campagna',
-            status: 'received'
-        },
-            {
-                date: '28/03/2020 10:20:10',
-                message: 'Sicuro di non aver sbagliato chat?',
-                status: 'sent'
+                name: 'Fabio',
+                avatar: 'avatar_2.jpg',
+                visible: true,
+                messages: [{
+                    date: '20/03/2020 16:30:00',
+                    message: 'Ciao come stai?',
+                    status: 'sent'
+                },
+                {
+                    date: '20/03/2020 16:30:55',
+                    message: 'Bene grazie! Stasera ci vediamo?',
+                    status: 'received'
+                },
+                {
+                    date: '20/03/2020 16:35:00',
+                    message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                    status: 'sent'
+                }
+                ],
             },
             {
-                date: '28/03/2020 16:15:22',
-                message: 'Ah scusa!',
-                status: 'received'
-            }
-        ],
-    },
-    {
-        name: 'Luisa',
-        avatar: '_4',
-        visible: true,
-        messages: [{
-            date: '10/01/2020 15:30:55',
-            message: 'Lo sai che ha aperto una nuova pizzeria?',
-            status: 'sent'
-        },
+                
+                name: 'Samuele',
+                avatar: 'avatar_3.jpg',
+                visible: true,
+                messages: [{
+                    date: '28/03/2020 10:10:40',
+                    message: 'La Marianna va in campagna',
+                    status: 'received'
+                },
+                {
+                    date: '28/03/2020 10:20:10',
+                    message: 'Sicuro di non aver sbagliato chat?',
+                    status: 'sent'
+                },
+                {
+                    date: '28/03/2020 16:15:22',
+                    message: 'Ah scusa!',
+                    status: 'received'
+                }
+                ],
+            },
             {
-                date: '10/01/2020 15:50:00',
-                message: 'Si, ma preferirei andare al cinema',
+                name: 'Luisa',
+                avatar: 'avatar_4.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
+            },
+           
+        ],
+        currentContact: 0,
+        newMessage: '',
+        lastDateContact: '',
+        searchContact: '',
+    },
+    
+    methods: { 
+
+        selected(i){
+            return this.currentContact = i;
+        },
+
+        lastDateContactCurrent(){
+
+            let contactMessages = this.contacts[this.currentContact].messages;
+
+            if (contactMessages[parseInt(contactMessages.length - 1)].status == "received")
+            contactLastAccess = contactMessages[parseInt(contactMessages.length - 1)].date;
+
+            else contactLastAccess = contactMessages[parseInt(contactMessages.length - 2)].date;
+        
+            return this.lastDateContact = contactLastAccess;
+        },
+        
+        UserNewMessage(){
+            let listMessages = this.contacts[this.currentContact].messages;
+
+            let messageUser = {
+                date: dayjs().format('DD/MM/YYYY hh:mm:ss'),
+                message: this.newMessage,
+                status: 'sent',
+            }
+
+            if (this.newMessage != '') {
+                listMessages.push(messageUser);
+                this.newMessage = '';
+            }
+
+            setInterval(this.ScrollTop);
+            setTimeout(this.messageReceived, 4000);
+        },
+
+        messageReceived(){
+            let listMessages = this.contacts[this.currentContact].messages;
+
+            let newMessageReceived = {
+                date:  dayjs().format('DD/MM/YYYY hh:mm:ss'),
+                message: 'ok',
                 status: 'received'
             }
-        ],
+
+            listMessages.push(newMessageReceived);
+        },
+
+        ScrollTop(){
+            let tmp = document.getElementById("chat_window");
+            tmp.scrollTop = tmp.scrollHeight;
+        },
+
+        filterSearch() {
+            return this.contacts.filter( contact => {
+            return !this.searchContact || contact.name.toLowerCase().indexOf(this.searchContact.toLowerCase()) > -1
+            })
+        },
+        
     },
-]
+    
+
+})
